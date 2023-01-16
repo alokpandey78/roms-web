8d7a45341e84baafc36dd5ffc652e73b240fcbe6
Kunal <Kunalsharmarok@gmail.com>
2023-01-16 19:33:28 +0530

    ip change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   localStorage: 'rtl',
   server: 'UAT',
-  apiUrl: 'http://test.rtl.com.au:8080',
-  socketApiUrl: 'http://test.rtl.com.au:8081',
+  apiUrl: 'http://3.111.205.172:8080',
+  socketApiUrl: 'http://3.111.205.172:8081',
   fileBaseUrl: 'http://localhost:8080/',
   imageUrl: 'http://localhost:8080/uploads/',
   orgId: "ab905406-79a3-4e54-8244-d79fc0e60937"
