c262dd05f6b16975af9f866e349dfe0700fe26d7
Kunal <Kunalsharmarok@gmail.com>
2023-02-23 22:05:21 +0530

    hazard report api intrigation

--- src/app/healthsafety/safetyhazard_details/safetyhazard_details.component.ts
+++ src/app/healthsafety/safetyhazard_details/safetyhazard_details.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { TemplateRef} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {ViewChild } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HealthsafetyService } from '../../core/services/healthsafety.service';
+import { first } from 'rxjs';
+import { Globals } from 'src/app/globals';
 
 @Component({
   selector: 'app-hazard-report',
@@ -6,10 +13,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./safetyhazard_details.component.scss']
 })
 export class SafetyhazardDetailsComponent implements OnInit {
+  submitted: boolean = false;
+  selectedRecord: any = {};
+  id : string = ''; 
+  hazardDetails : any = {};
+  globals: Globals;
 
-  constructor() { }
+  constructor(private dialog: MatDialog,
+    private router: Router,globals: Globals, private activatedRoute: ActivatedRoute, private healthsafetyService: HealthsafetyService,) { this.globals = globals;}
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.id = params['id'];
+    });
+    this.getHazardDetails();
+  }
+
+
+  getHazardDetails(){
+    this.healthsafetyService.getHazardDetails(this.id).pipe(first())
+        .subscribe((result: any) => {
+          this.hazardDetails = result.data;
+          console.log('Hazard Details:',this.hazardDetails);
+        });
   }
 
 }
