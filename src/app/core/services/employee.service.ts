ba1ec3c747457b1b85b750633d69e33dc478f874
Alok Pandey <abhishek.sharma@vmlyr.com>
2023-03-06 13:23:50 +0530

    Now admin can change the particular employee manager

--- src/app/core/services/employee.service.ts
+++ src/app/core/services/employee.service.ts
@@ -1,100 +1,110 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { ViewOptions } from "src/app/_models";
-import { environment } from "src/environments/environment";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ViewOptions } from 'src/app/_models';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class EmployeeService {
+  constructor(private http: HttpClient) {}
+  getAll(options: ViewOptions) {
+    return this.http.get<any>(
+      `${environment.apiUrl}/v1/employee/load?page=${options.page}&size=${options.pageSize}&${options.query}`,
+    );
+  }
 
-    constructor(private http: HttpClient) { }
-    getAll(options: ViewOptions) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/employee/load?page=${options.page}&size=${options.pageSize}&${options.query}`);
-    }
+  uploadPicture(data: any) {
+    // let headers = new HttpHeaders();
+    // headers.set('content-type', 'multipart/form-data');
+    return this.http.post<any>(`${environment.apiUrl}/v1/employee/uploadpic`, data);
+  }
 
+  getById(id: string) {
+    return this.http.get<any>(`${environment.apiUrl}/v1/employee/load/${id}`);
+  }
 
-    uploadPicture(data: any) {
-        // let headers = new HttpHeaders();
-        // headers.set('content-type', 'multipart/form-data');
-        return this.http.post<any>(`${environment.apiUrl}/v1/employee/uploadpic`, data);
-    }
+  getByIdPersonalInfo(id: string) {
+    return this.http.get<any>(`${environment.apiUrl}/v1/employee/personalinfo/${id}`);
+  }
 
-    getById(id: string) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/employee/load/${id}`);
-    }
+  searchEmployee(search: string) {
+    return this.http.get<any>(`${environment.apiUrl}/v1/subteammember/search?name=${search}`);
+  }
 
+  searchHiringManager(search: string) {
+    return this.http.get<any>(`${environment.apiUrl}/v1/employee/manager?name=${search}`);
+  }
 
-    getByIdPersonalInfo(id: string) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/employee/personalinfo/${id}`);
-    }
+  searchGang(search: string) {
+    return this.http.get<any>(`${environment.apiUrl}/v1/projectsubteam/search?name=${search}`);
+  }
 
+  getAllResignationRequest(options: ViewOptions) {
+    return this.http.get<any>(
+      `${environment.apiUrl}/v1/resignation/appliedToMe?status=${options.search}&page=${options.page}&size=${options.pageSize}`,
+    );
+  }
 
-    searchEmployee(search: string) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/subteammember/search?name=${search}`);
-    }
+  resignationHistory(options: ViewOptions) {
+    return this.http.get<any>(
+      `${environment.apiUrl}/v1/resignation/appliedToMeHistory?page=${options.page}&size=${options.pageSize}`,
+    );
+  }
 
-    searchHiringManager(search: string) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/employee/manager?name=${search}`);
-    }
-    
-    searchGang(search: string) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/projectsubteam/search?name=${search}`);
-    }
+  acceptResignation(data: any) {
+    return this.http.post<any>(`${environment.apiUrl}/v1/resignation/approve`, data);
+  }
 
+  rejectResignation(data: any) {
+    return this.http.post<any>(`${environment.apiUrl}/v1/resignation/reject`, data);
+  }
 
-    getAllResignationRequest(options: ViewOptions) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/resignation/appliedToMe?status=${options.search}&page=${options.page}&size=${options.pageSize}`);
-    }
+  employeeOnboardList(options: ViewOptions) {
+    return this.http.get<any>(
+      `${environment.apiUrl}/v1/employee/onboard/loadAll?page=${options.page}&size=${options.pageSize}`,
+    );
+  }
 
+  getAllEmployeeResignation(options: ViewOptions, queryData: any) {
+    return this.http.post<any>(
+      `${environment.apiUrl}/v1/resignation/loadAll?page=${options.page}&size=${options.pageSize}`,
+      queryData,
+    );
+  }
 
-    resignationHistory(options: ViewOptions) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/resignation/appliedToMeHistory?page=${options.page}&size=${options.pageSize}`);
-    }
+  searchEmployeeByName(value: string) {
+    return this.http.get<any>(`${environment.apiUrl}/v1/subteammember/search?name=${value}`);
+  }
 
-    acceptResignation(data: any) {
-        return this.http.post<any>(`${environment.apiUrl}/v1/resignation/approve`, data);
+  searchEmployeeForJobDemand(value: string) {
+    return this.http.get<any>(
+      `${environment.apiUrl}/v1/employee/load?page-0&size=800&empName=${value}`,
+    );
+  }
+  searchEmployeeUnderManager(value: string) {
+    return this.http.get<any>(`${environment.apiUrl}/v1/employee/load/own?empName=${value}`);
+  }
 
-    }
+  getEmployeeGangDetails(id: string) {
+    return this.http.get<any>(`${environment.apiUrl}/v1/subteammember/${id}`);
+  }
 
-    rejectResignation(data: any) {
-        return this.http.post<any>(`${environment.apiUrl}/v1/resignation/reject`, data);
-    }
+  resetPasswordById(employeeId: string) {
+    return this.http.get<any>(`${environment.apiUrl}/v1/password/reset/${employeeId}`);
+  }
 
+  recommendEmployee(param: any) {
+    return this.http.post<any>(`${environment.apiUrl}/v1/jobs/resource/demand/recommend`, param);
+  }
+  getEmployeeList(options: ViewOptions, payload: any) {
+    return this.http.get<any>(
+      `${environment.apiUrl}/v1/employee/load?page=${options.page}&size=${options.pageSize}&empName=${payload.searchText}&payfrequency=${payload.frequency}&empStatus=${payload.empStatus}`,
+    );
+  }
 
-    employeeOnboardList(options: ViewOptions) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/employee/onboard/loadAll?page=${options.page}&size=${options.pageSize}`);
-    }
-
-
-
-
-    getAllEmployeeResignation(options: ViewOptions, queryData: any) {
-        return this.http.post<any>(`${environment.apiUrl}/v1/resignation/loadAll?page=${options.page}&size=${options.pageSize}`, queryData);
-    }
-
-    searchEmployeeByName(value: string) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/subteammember/search?name=${value}`);
-    }
-
-    searchEmployeeForJobDemand(value: string) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/employee/load?page-0&size=800&empName=${value}`);
-    }
-    searchEmployeeUnderManager(value: string) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/employee/load/own?empName=${value}`);
-    }
-
-    getEmployeeGangDetails(id: string) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/subteammember/${id}`);
-    }
-
-    resetPasswordById(employeeId: string) {
-        return this.http.get<any>(`${environment.apiUrl}/v1/password/reset/${employeeId}`);
-    }
-
-    recommendEmployee(param:any) {
-        return this.http.post<any>(`${environment.apiUrl}/v1/jobs/resource/demand/recommend`, param);
-    }
-    getEmployeeList(options: ViewOptions, payload: any){
-        return this.http.get<any>(`${environment.apiUrl}/v1/employee/load?page=${options.page}&size=${options.pageSize}&empName=${payload.searchText}&payfrequency=${payload.frequency}&empStatus=${payload.empStatus}`);
-    }
-
+  getManagerdata() {
+    return this.http.get<any>(`${environment.apiUrl}/v1/employee/manager?name`);
+  }
+  setmanagerdata(setmanagerdata: any) {
+    return this.http.put<any>(`${environment.apiUrl}/v1/employee/manager`, setmanagerdata);
+  }
 }
--- src/app/employee/employee-view/employee-view.component.ts
+++ src/app/employee/employee-view/employee-view.component.ts
@@ -3,21 +3,28 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AlertService, EmployeeService } from 'src/app/core/services';
 import { AuthenticationService } from 'src/app/core/services/auth.service';
 import { CustomMessage } from 'src/app/custom-message';
+import { FormGroup } from '@angular/forms';
 import { Globals } from 'src/app/globals';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-employee-view',
   templateUrl: './employee-view.component.html',
-  styleUrls: ['./employee-view.component.css']
+  styleUrls: ['./employee-view.component.css'],
 })
 export class EmployeeViewComponent implements OnInit {
   globals: Globals;
   panelOpenState = false;
   step = 0;
   record: any;
+  data: any;
   user: any;
-  id: string = "";
+  managerdata: any;
+  test: string = 'hello';
+  id: string = '';
   currentAddress: any;
+  setmngrdata: any;
   postalAddress: any;
   residentialAddress: any;
   isEditEnable: boolean = false;
@@ -34,8 +41,24 @@ export class EmployeeViewComponent implements OnInit {
   paymentFrequency: string = '';
   type: string = 'personal';
   tabIndex: number = 0;
-  tabIndexList: any = ['personal', 'emergency', 'licence', 'payroll', 'leaves', 'membership', 'employment'];
-  constructor(private activatedRoute: ActivatedRoute, private employeeService: EmployeeService, private router: Router, private alertService: AlertService, private authService: AuthenticationService, global: Globals) {
+  tabIndexList: any = [
+    'personal',
+    'emergency',
+    'licence',
+    'payroll',
+    'leaves',
+    'membership',
+    'employment',
+  ];
+  constructor(
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute,
+    private employeeService: EmployeeService,
+    private router: Router,
+    private alertService: AlertService,
+    private authService: AuthenticationService,
+    global: Globals,
+  ) {
     this.globals = global;
     this.user = this.authService.getCurrentUser();
 
@@ -54,61 +77,123 @@ export class EmployeeViewComponent implements OnInit {
       }
       if (params['id']) {
         this.id = params['id'];
-        this.employeeService.getByIdPersonalInfo(params['id']).subscribe((result: any) => {
-          this.record = result;
-          if (this.record && this.record.address) {
-            this.currentAddress = this.record.address.find((elem: any) => elem.type == 2);
-            this.postalAddress = this.record.address.find((elem: any) => elem.type == 1);
-            this.residentialAddress = this.record.address.find((elem: any) => elem.type == 1);
-            this.emergencyContactMain = this.record.emergencyContact && this.record.emergencyContact.length > 0 ? this.record.emergencyContact[0] : null;
-            this.otherEmergencyContact = this.record.emergencyContact && this.record.emergencyContact.length > 1 ? this.record.emergencyContact[1] : null;
+        this.employeeService.getByIdPersonalInfo(params['id']).subscribe(
+          (result: any) => {
+            console.log(this.user);
+            this.record = result;
+            if (this.record && this.record.address) {
+              console.log(this.record);
+              this.currentAddress = this.record.address.find((elem: any) => elem.type == 2);
+              this.postalAddress = this.record.address.find((elem: any) => elem.type == 1);
+              this.residentialAddress = this.record.address.find((elem: any) => elem.type == 1);
+              this.emergencyContactMain =
+                this.record.emergencyContact && this.record.emergencyContact.length > 0
+                  ? this.record.emergencyContact[0]
+                  : null;
+              this.otherEmergencyContact =
+                this.record.emergencyContact && this.record.emergencyContact.length > 1
+                  ? this.record.emergencyContact[1]
+                  : null;
 
-            this.primaryLicence = this.record.licence && this.record.licence.length > 0 ? this.record.licence[0] : null;
-            this.otherLicence = this.record.licence && this.record.licence.length > 1 ? this.record.licence[1] : null;
+              this.primaryLicence =
+                this.record.licence && this.record.licence.length > 0
+                  ? this.record.licence[0]
+                  : null;
+              this.otherLicence =
+                this.record.licence && this.record.licence.length > 1
+                  ? this.record.licence[1]
+                  : null;
 
+              this.primaryBank =
+                this.record.bankingDetails && this.record.bankingDetails.length > 0
+                  ? this.record.bankingDetails[0]
+                  : null;
+              this.otherBank =
+                this.record.bankingDetails && this.record.bankingDetails.length > 1
+                  ? this.record.bankingDetails[1]
+                  : null;
 
-            this.primaryBank = this.record.bankingDetails && this.record.bankingDetails.length > 0 ? this.record.bankingDetails[0] : null;
-            this.otherBank = this.record.bankingDetails && this.record.bankingDetails.length > 1 ? this.record.bankingDetails[1] : null;
+              this.primaryTfn =
+                this.record.tfn && this.record.tfn.length > 0 ? this.record.tfn[0] : null;
 
-            this.primaryTfn = this.record.tfn && this.record.tfn.length > 0 ? this.record.tfn[0] : null;
+              this.primarySuperAnnuation =
+                this.record.superannuation && this.record.superannuation.length > 0
+                  ? this.record.superannuation[this.record.superannuation.length - 1]
+                  : null;
+              if (
+                this.record.superannuation.length >= 1 &&
+                this.record.superannuation[0] &&
+                this.record.superannuation[0].abn
+              ) {
+                this.primarySuperAnnuation = this.record.superannuation[0];
+              } else if (
+                this.record.superannuation.length >= 2 &&
+                this.record.superannuation[1] &&
+                this.record.superannuation[1].abn
+              ) {
+                this.primarySuperAnnuation = this.record.superannuation[1];
+              } else if (this.record.superannuation.length > 0) {
+                this.primarySuperAnnuation = {
+                  fundType: 3,
+                  usi: this.globals.defaultUsi,
+                  abn: this.globals.defaultAbn,
+                };
+              }
 
-            this.primarySuperAnnuation = this.record.superannuation && this.record.superannuation.length > 0 ? this.record.superannuation[this.record.superannuation.length - 1] : null;
-            if (this.record.superannuation.length >= 1 && this.record.superannuation[0] && this.record.superannuation[0].abn) {
-              this.primarySuperAnnuation = this.record.superannuation[0];
-            } else if (this.record.superannuation.length >= 2 && this.record.superannuation[1] && this.record.superannuation[1].abn) {
-              this.primarySuperAnnuation = this.record.superannuation[1];
-            } else if (this.record.superannuation.length > 0) {
-              this.primarySuperAnnuation = { fundType: 3, usi: this.globals.defaultUsi, abn: this.globals.defaultAbn };
+              this.otherSuperAnnuation =
+                this.record.superannuation && this.record.superannuation.length > 1
+                  ? this.record.superannuation[1]
+                  : null;
+              // this.primarySuperAnnuation = this.otherSuperAnnuation ? this.otherSuperAnnuation : this.otherSuperAnnuation;
 
+              this.primaryMembership =
+                this.record.membership && this.record.membership.length > 0
+                  ? this.record.membership[0]
+                  : null;
+              this.paymentFrequency =
+                this.record?.personal?.paymentFrequency == 'w'
+                  ? 'Weekly'
+                  : this.record?.personal?.paymentFrequency == 'm'
+                  ? 'Monthly'
+                  : 'Fortnightly';
             }
-
-            this.otherSuperAnnuation = this.record.superannuation && this.record.superannuation.length > 1 ? this.record.superannuation[1] : null;
-            // this.primarySuperAnnuation = this.otherSuperAnnuation ? this.otherSuperAnnuation : this.otherSuperAnnuation;
-
-            this.primaryMembership = this.record.membership && this.record.membership.length > 0 ? this.record.membership[0] : null;
-            this.paymentFrequency = this.record?.personal?.paymentFrequency == 'w' ? 'Weekly' : (this.record?.personal?.paymentFrequency == 'm' ? 'Monthly' : 'Fortnightly');
-          }
-          console.log(result);
-        }, (error: any) => {
-          this.alertService.openSnackBar(CustomMessage.recordNotFound);
-          this.router.navigate(['/dashboard']);
-        });
+            console.log(result);
+          },
+          (error: any) => {
+            this.alertService.openSnackBar(CustomMessage.recordNotFound);
+            this.router.navigate(['/dashboard']);
+          },
+        );
       } else {
         this.router.navigate(['/dashboard']);
       }
-    })
+    });
   }
 
   ngOnInit(): void {
+    this.getmanager();
   }
 
   redirect() {
-    console.log(this.user)
+    console.log(this.user);
     if (this.user.id == this.id) {
       this.router.navigate(['/profile']);
     } else {
       this.router.navigate(['/employee/profile'], { queryParams: { id: this.id } });
-
     }
+  }
+  public getmanager() {
+    this.employeeService.getManagerdata().subscribe((mdata) => {
+      this.managerdata = mdata;
+    });
+  }
+
+  setmanager(mid: string) {
+    this.setmngrdata = {
+      employeeIds: this.user.id,
+      managerId: mid,
+    };
+
+    this.employeeService.setmanagerdata(this.setmngrdata).subscribe();
   }
 }
